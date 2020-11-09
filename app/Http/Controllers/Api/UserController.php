b2f882659100d548a96a72a3c59789036e454354
namduongkiwi <namduongkiwi@gmail.com>
2020-11-10 00:04:40 +0700

    update api login, register

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -19,11 +19,8 @@ class UserController extends Controller
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
-            /*$success['token'] =  $user->createToken('MyApp')->accessToken;
-            $token = $success['token'];
-            $user->forceFill([
-                'remember_token' => $token,
-            ])->save();*/
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            //$token = $success['token'];
             $success['id'] = Auth::id();
             return response()->json(['success' => $success], $this-> successStatus);
         }
